fixed accidental minus sign in last commit that inverted the proton spectrum!

gammasens/spectra.py
@@ -7,7 +7,7 @@ import numpy as np
 from math import pi
 from astropy import units
 
-def powerlaw( E, index, norm, norm_energy ):
+def powerlaw( E, index, norm, norm_energy=1.0 ):
     return norm*(E/norm_energy)**(-index)
 
 def lognormal( E, center, width ):
@@ -31,7 +31,7 @@ def electron_spectrum(e_true_tev):
 def electron_spectrum_powerlaw(e_true_tev):
     """ simple power-law cosmic-ray electron spectrum """
     norm = units.Quantity(6.85e-5, "1/(TeV s m**2 sr)") 
-    return powerlaw( e_true_tev, index=3.21, norm=norm, norm_energy=1 )
+    return powerlaw( e_true_tev, index=3.21, norm=norm, norm_energy=1.0 )
 
 
 def electron_spectrum_fermi(e_true_tev, E_peak_tev=0.107, width=0.776):
@@ -60,7 +60,7 @@ def proton_spectrum(e_true_tev):
     """
 
     norm = units.Quantity(0.096,"TeV**-1 s**-1 m**-2 sr**-1")
-    return powerlaw(e_true_tev, norm=norm, index=-2.7,norm_energy=1.0)
+    return powerlaw(e_true_tev, norm=norm, index=2.7,norm_energy=1.0)
 
 def cosmicray_spectrum(e_true_tev):
     """
@@ -74,7 +74,8 @@ def cosmicray_spectrum(e_true_tev):
 
 def hess_crab_spectrum(e_true_tev, fraction=1.0) :
     norm = fraction*units.Quantity(3.76e-11, "ct cm**-2 s**-1 TeV**-1")
-    return  powerlaw( e_true_tev, norm=norm, index=2.39)* np.exp(-e_true_tev/14.3)
+    return  powerlaw( e_true_tev, norm=norm,
+                      index=2.39, norm_energy=1.0) * np.exp(-e_true_tev/14.3)
 
 def hess_binned_crab_spectrum(logEmin, logEmax, fraction=1.0):
     spec = np.zeros_like(logEmin)