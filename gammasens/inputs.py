fixed bug in loading old-style CTA sensitivity inputs. Output now agrees with internal calculation

gammasens/inputs.py
@@ -337,14 +337,24 @@ def load_cta_response(filename):
     pre-processed (e.g. background rate is already calculated). This
     is included for comparing older CTA sensitivity output to those
     calculated with ParticleDistributions
+    
+    Assumes the text file has the columns:
+    * 0: log10(E) at the bin center (TeV)
+    * 1: A_eff for gammas, after all cuts  (m^2)
+    * 2-4: r28,r80,Eres (not used)
+    * 5: background rate post-cuts, and inside source region (Hz)
+    * 6: differential sensitivity pre-calculated, E^2 dN/dE (erg/cm^2/s)
     """
     AA = np.loadtxt(filename)
     log_e = AA[:,0]
-    delta_log_e = log_e[1]-log_e[0]
-    delta_e = 10**(np.ones_like(AA[:,0]) * delta_log_e)
+    dloge = log_e[1]-log_e[0]
+
+    log_e_lo = log_e - dloge/2.0
+    log_e_hi = log_e + dloge/2.0
+
     sens = AA[:,6]
 
-    return (log_e, delta_e * units.TeV, AA[:,1] *units.m**2,
+    return (log_e_lo, log_e_hi, AA[:,1] *units.m**2,
             AA[:,5] * units.count/units.s, 
             units.Quantity(AA[:,6],"erg/(cm**2 s)") )
     

gammasens/sensitivity.py
@@ -17,6 +17,13 @@ from gammasens import spectra
 EPSILON = 1.0e-10
 PSTY = {'linestyle':'steps-mid', 'linewidth':2} # default line style    
 
+class SensOutput(dict):
+    """ just a dict that allows tab completion in ipython """
+    def __init__(self,**kw):
+        dict.__init__(self,kw)
+        self.__dict__ = self
+
+
 
 def solid_angle( theta ):
     """
@@ -181,13 +188,18 @@ def calc_sensitivity(name, background_rate, gamma_aeff_reco, delta_e,
     N_on_orig = N_on.copy()
     mask = excess(N_on,N_off,alpha)<min_events
     if any(mask):
+#        print "EXC:",len(mask)
+#        print excess(N_on,N_off,alpha)[mask]
         N_on[mask] = min_events + alpha[mask]*N_off[mask]
+#        print excess(N_on,N_off,alpha)[mask]
 
     # apply conditions on minimum background systematics
     mask = excess(N_on,N_off,alpha)<N_off*min_sys_pct*0.01
     if any(mask):
+#        print "SYS:",len(mask)
+#        print excess(N_on,N_off,alpha)[mask]
         N_on[mask] = N_off[mask]*min_sys_pct*0.01 + alpha[mask]*N_off[mask]
-           
+#        print excess(N_on,N_off,alpha)[mask]
     
     # calculate sensitivity limit, and conver to proper units 
     N_on[np.isnan(N_off)] = np.nan  #chop off bad values
@@ -204,7 +216,7 @@ def calc_sensitivity(name, background_rate, gamma_aeff_reco, delta_e,
 
     # return all the output (including intermediate values) in a dict,
     # for later plotting
-    return dict( params=dict(obstime=obstime,
+    return SensOutput( params=dict(obstime=obstime,
                               num_bg_regions = num_bg_regions,
                               min_signif=min_signif,
                               min_events=min_events,
@@ -270,17 +282,21 @@ def plot_rates( log_e, rate_p, rate_e, sens ):
     plt.xlabel("log E$_{reco}$/TeV")
     plt.legend( loc="best")
 
-def plot_sensitivity(log_e, sens, **kwargs):
+def plot_sensitivity(log_e, sens, esquared=False, **kwargs):
     """
     Display the differential sensitivity curve 
 
     :param sens: sensitivity output dictrionary
     """
 
+    E = 10**log_e * units.TeV
     
     sensitivity = sens['sensitivity']
-    par = sens['params']
 
+    if (esquared):
+        sensitivity *= E**2
+
+    par = sens['params']
     label=r"{2} {0}, {1} $\sigma$".format(par['obstime'].to(units.h), 
                                              par['min_signif'], sens['name'])
 